3b6de7573f490189222c331ab717a03af81bf04c
Christopher Rogers <slaxemulator@gmail.com>
2017-04-24 14:46:27 +0000

    Add zimwriterfs

---
+++ ap/zimwriterfs/grab.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+pkgname=zimwriterfs
+giturl="https://github.com/openzim/zimwriterfs"
+
+# set pkgver to current date
+pkgver=$(date +%Y%m%d)
+if [ -d $pkgname ]; then
+	(
+	cd $pkgname
+	git pull --depth 1
+	)
+else
+	git clone --depth 1 $giturl
+fi
+ 
+# create the source archive
+tar czf $pkgname-$pkgver.src.tar.gz $pkgname
+ 
+# change the SLKBUILD
+sed -i 's#^pkgver=.*#pkgver='$pkgver'#' SLKBUILD
